6c93e2d56d5cc5c8e023a11ea42185d52e90129f
Stanislav <qixi@mail.ru>
2023-08-02 08:52:18 +0300

    added DataRepository

---
+++ core/src/commonMain/kotlin/qixi/propio/repository/DataRepository.kt
@@ -0,0 +1,60 @@
+package qixi.propio.repository
+
+import kotlinx.io.Sink
+import kotlinx.io.files.Path
+import kotlinx.io.files.source
+import qixi.propio.MutablePreferences
+import qixi.propio.Preferences
+import qixi.propio.mutablePreferencesOf
+import qixi.propio.readFrom
+import qixi.propio.writeTo
+
+abstract class DataRepository<K, V> {
+
+    protected val repository = mutableMapOf<K, V>()
+    val data: Collection<V> = repository.values
+    val size: Int get() = repository.size
+
+    operator fun contains(key: K): Boolean = repository.containsKey(key)
+
+    operator fun get(key: K): V? = repository[key]
+
+    protected abstract fun getPath(data: V): Path?
+
+    protected abstract fun createSink(data: V): Sink
+
+    protected abstract fun removeSource(data: V)
+
+    protected abstract fun readData(data: V, prop: Preferences)
+
+    protected abstract fun writeData(prop: MutablePreferences, data: V)
+
+    fun add(key: K, value: V?) {
+        if (value == null) {
+            remove(key)
+        } else {
+            repository[key] = value
+            save(value)
+        }
+    }
+
+    fun remove(key: K) {
+        val data = repository.remove(key)
+        if (data != null) removeSource(data)
+    }
+
+    open fun load(data: V): Boolean {
+        val filePath = getPath(data) ?: return false
+        val prop = mutablePreferencesOf()
+        prop.readFrom(filePath.source())
+        readData(data, prop)
+        return true
+    }
+
+    open fun save(data: V) {
+        val prop = mutablePreferencesOf()
+        writeData(prop, data)
+        prop.writeTo(createSink(data))
+    }
+
+}
