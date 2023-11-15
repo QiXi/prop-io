dd626e9d0308669ebb763b87c946304ec62c8999
Stas <qixiru@gmail.com>
2023-11-15 04:33:32 +0300

    fix gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "prop-io"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":core")
+//include(":loader")
